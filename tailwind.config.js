7eadcdf14179e8d865fa1e890ec76c7d525dc910
Kazk12 <kazuko43KO@gmail.com>
2024-10-30 15:08:45 +0100

    ADD:NAV SECTION 1 ET 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html"
+    './index.html',
   ],
   theme: {
     extend: {
-      layout:{
-        mobile :"375px",
-        desktop: "1440px"
-      },
-      color: {
+      colors: {
         primary: {
           orange: 'hsl(35, 77%, 62%)',
           red: 'hsl(5, 85%, 63%)',
         },
         neutral: {
           'off-white': 'hsl(36, 100%, 99%)',
-          'dark-grayinsh-blue':'hsl(236, 13%, 42%)',
-          'Grayish-blue': 'hsl(233, 8%, 79%)',
-          'Very dark blue': 'hsl(240, 100%, 5%)',
+          'grayish-blue': 'hsl(233, 8%, 79%)',
+          'dark-grayish-blue': 'hsl(236, 13%, 42%)',
+          'very-dark-blue': 'hsl(240, 100%, 5%)',
         },
       },
-      typography:{
-        bodycopy: {
-          "Font-size":"(paragraph): 15px",
-        },
-        font: {
-          Family: "[Inter](https://fonts.google.com/specimen/Inter)",
-          Weights : "400, 700, 800",
-        }
+      fontFamily: {
+        body: ['Inter', 'sans-serif'],
+      },
+      fontSize: {
+        paragraph: '20px',
       },
     },
   },
   plugins: [],
 }
-
